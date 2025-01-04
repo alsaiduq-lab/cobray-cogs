37a17c697b0243453484ec7a6c60f71a0ee0d420
alsaiduq-lab <riiidge.racer@gmail.com>
2025-01-04 14:00:30 -0700

    whoops

--- booru/sources/__init__.py
+++ booru/sources/__init__.py
@@ -3,6 +3,7 @@ from .gelbooru import GelbooruSource
 from .yandere import YandereSource
 from .konachan import KonachanSource
 from .safebooru import SafebooruSource
+from .r34 import Rule34Source
 
 __all__ = [
     "DanbooruSource",
