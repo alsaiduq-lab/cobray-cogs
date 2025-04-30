29a7acce3858262b6f7743464d968a5ccfcfbd34
alsaiduq-lab <riiidge.racer@gmail.com>
2025-04-30 05:08:13 -0600

    update

--- booru/__init__.py
+++ booru/__init__.py
@@ -1,9 +1,11 @@
 from redbot.core.bot import Red
 
 from .booru import Booru
+from core.slash import BooruSlash
 
 __red_end_user_data_statement__ = "This cog does not store end user data."
 
 
 async def setup(bot: Red):
     await bot.add_cog(Booru(bot))
+    await bot.add_cog(BooruSlash(bot))
